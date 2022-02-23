a3da64f07e8d650262a4521d70561e796fa5ea48
Michal Janecek <michal.janecek@gmail.com>
2022-02-23 15:11:26 +0100

    mfix

--- Druhá lekce/Druhá lekce.py
+++ Druhá lekce/Druhá lekce.py
@@ -197,6 +197,17 @@ Načti od uživatele 3 čísla:
     - 1. číslo je počet řádek obrazce
     - 2. číslo je šířka obrazce
     - 3. číslo je počet hvězdiček na řádku (a zbytek jsou #), nejdřív jsou vždy * pak až #
+(Pozor, počet hvězdiček logicky nesmí být větší, než šířka řádku... tak pozor až budete zadávat čísla)
+
+Např: pro čísla 5 (počet řádků), 10 (šířka) a 2 (počet hvězd) by se mělo ukázat toto:
+
+**########
+**########
+**########
+**########
+**########
+
+
 """
 
 radku = int(input())
