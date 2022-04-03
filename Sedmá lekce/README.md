c3d9e5dce30a0453f22d624535d645d3fc8119aa
Michal Janecek <michal.janecek@gmail.com>
2022-04-03 23:26:16 +0200

    Nové úlohy na while, rozpracovaná příprava na 7. lekci. Zbývá hlavně dopsat řešení + udělat soubor py pro 7 lekci

---
+++ Sedmá lekce/README.md
@@ -0,0 +1,14 @@
+## Plán 7. lekce
+
+1. Kvíz
+2. Odpovědi na otázky ve kvízu
+3. První cyklus!
+   1. Co je to while cyklus
+   2. Syntax (jak se to píše)
+   3. **SPOLEČNĚ** pár příkladů
+4. Samostatné úlohy
+5. Operátor **modulo**
+
+
+Někdo bude ještě dělat nějaké úložky na podmínky. *Cyklus while* se ale budeme snažit probrat všichni společně, pak už každej 
+pojede svoje tempo. Cyklům se ještě budeme věnovat (každou hodinu :D), takže není kam spěchat.
--- Témata/cyklus_while.py
+++ Témata/cyklus_while.py
@@ -1,6 +1,6 @@
-# Programy, co jsme zatím dělali měly jedno společné - byly poměrně jednoduché v tom, že se každý příkaz vykonal maximálně jednou.
-# Např. pokud jsme udělaly kalkulačku, tak na jedno spuštění programu jsme mohli vypočítat jen jeden příklad.
-# Pro spočítání 100 příkladů bychom program museli pustit 100x...
+# Programy, co jsme zatím dělali, měly jedno společné - byly poměrně jednoduché v tom, že se každý příkaz vykonal maximálně jednou.
+# Např. pokud jsme udělali kalkulačku, tak na jedno spuštění programu jsme mohli vypočítat jen jeden příklad.
+# Pro spočítání 100 příkladů bychom program museli pustit 100x... a nebo 100x za sebou napsat ten stejný kód, což by bylo velmi nepěkné.
 # Co kdybychom chtěli, aby se program opakoval? K tomu máme CYKLUS!
 
 # CYKLUS WHILE
@@ -8,6 +8,11 @@
 # Je to hodně podobné jako IF, akorát se příkazy nevykonají jen jednou, ale budou se vykonávat pořád dokola, dokud je ta
 # podmínka platná.
 
+# Představte si podmínku (if). Pokud předpoklad platí, tak se vykonají příkazy, co jsou pod podmínkou (python to pozná
+# podle odsazení. Cyklus WHILE (dokud) je velmi podobný, akorát se příkazy (pokud platí podmínka) nevykonají jen jednou,
+# ale budou se dělat tak dlouho, dokud podmínka bude platit. Takže python normálně od shora dolů vykonává příkazy a když dorazí
+# na konec cyklu (tam, kde končí odsazení), znovu zkontroluje podmínku a když pořád platí, tak znova vykonává příkazy od shora dolu.
+# A takhle pořád dokola.
 
 # Chceme po uživateli, aby řekl Ááá. Když se mu to nepovede, nechceme aby program skončil, ale dáme mu další šanci.
 # A tak pořád dokola, dokud neřekne Ááá.
@@ -20,7 +25,31 @@ while odpoved != 'Ááá':
 
 # zdroj: https://naucse.python.cz/lessons/beginners/while/
 
-# Co když je podmínka splněna vždy?
+# Všimněte si, že proměnná co se používá v podmínce se v cyklu mění. Kdyby se neměnila, tak by cyklus nikdy neskončil.
+# Jak se bude chovat tento program, když uživatel zada věk menší než 18 ?
+vek = int(input("Zadej svůj věk pro ověření, zda-li můžeš vstoupit do baru."))
+while vek < 18:
+    print("Zadal jsi moc nízký věk. ")
+    print("Zkus to znovu.")
+
+# Nikdy neskončí a bude neustále psát to samé. Kde je chyba?
+# Podmínka je sice hezká, ale my se uživatele na věk už nikdy nezeptáme a hodnota věku zůstane už na furt stejná.
+# No a proto se hodnota podmínky nikdy nezmění a program zůstane do nekonečna zaseklý.
+
+
+# Někdy se ale může hodit mít nekonečný cyklus, co když chci třeba něco dělat každou sekundu?
+# Využijeme funkci sleep (spi), které řekneme, kolik sekund má program spát - zastavit se.
+import time
+tik = True
+while True:
+    if tik==True:
+        print("Tik")
+    else:
+        print("Tak")
+    tik = not tik
+    time.sleep(1)
+
+
 from random import randrange
 while True:
     print('Číslo je', randrange(10000))
@@ -39,6 +68,7 @@ while n <= 10: # Dokud je n menší nebo rovno 10, budu přičítat do součtu. 
 print("Součet čísel od 1 do 10 je: " + str(soucet))
 
 
+
 """
 Úkol 1: Součet čísel od 50 do 100
 
@@ -46,10 +76,51 @@ Pomocí cyklu while udělejte program, který spočítá součet čísel od 50 d
 
 """
 
+"""
+Úkol 2: Fibonacciho čísla
+
+Zobrazte prvních 100 čísel Fibonacciho posloupnosti.
+Začíná se s 0 a 1, a každé další číslo je součtem dvou předchozích.
+Takže 0, 1, 1, 2, 3, 5 ... 
+"""
+
+"""
+Úkol 3: Součty sudých a lichých čísel
+
+Pomocí cyklu while spočítejte a zobrazte součet čísel od 1 do 100 (včetně).
+Ale POZOR, nezajímá nás celkový součet, ale součet sudých a lichých čísel zvlášť!
+Použijte operátor modulo (zbytek po dělení).
+"""
+
+"""
+Úkol 4: Polopyramida
+
+Zeptejte se uživatele na výšku pyramidy a vypište mu tak vysokou polopyramidu z hvězdiček.
+Například polopyramida výšky 5 vypadá takto:
+*
+**
+***
+****
+*****
 
 """
 
-Úkol 10: Uhodni číslo
+
+"""
+Úkol 4.5: Legit pyramida (pro rychlíky/na doma)
+
+Stejný úkol jako polopyramida, akorát že to není polopyramida, ale legit pyramida. 
+Například legit pyramida výšky 5 vypadá takto:
+    *
+   ***
+  *****
+ *******
+*********
+"""
+
+"""
+
+Úkol 5: Uhodni číslo
 
 Udělej hru, kde uživatel bude hádat číslo, které si program myslí. 
 Uživatel bude dávat tipy (má jich omezený počet, třeba 10) a pokud číslo uhodne, program končí.
@@ -73,3 +144,12 @@ if tip == cislo:
     print("Gratuluji, myslel jsem si " + str(tip) + ". Uhádl si!")
 else:
     print("Prohrál jsi... Myslel jsem si " + str(cislo) + " .")
+
+
+"""
+
+Úkol 6: Hledání prvočísel (bonus)
+
+TODO
+
+"""
